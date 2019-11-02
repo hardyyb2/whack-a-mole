bad2ce22322585f806e86ea4d5ee1fb41190b6e4
Hardik Badola <hardikb3732@gmail.com>
2019-11-02 11:52:55 +0530

    reset added

--- public/index.js
+++ public/index.js
@@ -1,59 +1,81 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+let lastHole
+let timeUp = false;
+let score = 0;
+let oldMaxScore = localStorage.getItem('maxscore');
+if(!oldMaxScore) oldMaxScore=0;
 
-    const holes = document.querySelectorAll('.hole');
-    const scoreBoard = document.querySelector('.score');
-    const moles = document.querySelectorAll('.mole');
-    let lastHole
-    let timeUp = false;
-    let score = 0;
-    let startButton = document.querySelector("#startbutton")
-
-    function randomTime(min, max) {
-      return Math.floor(Math.random() * (max - min) + min);
-    }
-
-    function randomHole(holes) {
-      const idx =Math.floor(Math.random() * holes.length)
-      const hole = holes[idx];
-      if (lastHole === hole) {
-        return randomHole(holes)
-      }
-      lastHole = hole
-      return hole;
-    }
+let startButton = document.querySelector("#startbutton")
+const maxScoreText = document.querySelector(`.max-score__item`);
+maxScoreText.textContent = oldMaxScore;
 
 
-    function peep() {
-      let time = randomTime(1000, 2000);
-      let hole = randomHole(holes);
-      hole.classList.add('up');
-      setTimeout(() => {
-        hole.classList.remove('up');
-        if (!timeUp) peep();
-      }, time)
-    }
+function randomTime(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
-    function startGame() {
-      score = 0;
-      scoreBoard.textContent = 0;
-      timeUp = false;
-      peep();
-      startButton.disabled = true;
-      setTimeout(() => {
-        timeUp = true
-        startButton.disabled = false;
-      }, 6000)
-    }
+function randomHole(holes) {
+  const idx = Math.floor(Math.random() * holes.length)
+  const hole = holes[idx];
+  if (lastHole === hole) {
+    return randomHole(holes)
+  }
+  lastHole = hole
+  return hole;
+}
 
-    function bonk(e) {
-      if (!e.isTrusted) return
-      score++;
-      scoreBoard.textContent = score;
-      this.parentNode.classList.remove('up');
-      this.classList.add('shaking')
-      setTimeout(()=>{
-        this.classList.remove('shaking')
-      },400)
-    }
 
-    moles.forEach(mole => mole.addEventListener('click', bonk))
-  
+function peep() {
+  let time = randomTime(1000, 2000);
+  let hole = randomHole(holes);
+  hole.classList.add('up');
+  setTimeout(() => {
+    hole.classList.remove('up');
+    if (!timeUp) peep();
+  }, time)
+}
+
+function startGame() {
+  score = 0;
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  peep();
+  startButton.disabled = true;
+  setTimeout(() => {
+    timeUp = true
+    startButton.disabled = false;
+    setMaxScore();
+  }, 6000)
+  setMaxScore();
+}
+
+ function bonk(e) {
+  if (!e.isTrusted) return
+  score++;
+  scoreBoard.textContent = score;
+  this.parentNode.classList.remove('up');
+  this.classList.add('shaking')
+   setTimeout(() => {
+    this.classList.remove('shaking')
+  }, 400)
+}
+
+
+moles.forEach(mole => mole.addEventListener('click', bonk))
+
+const resetMaxScore = () => {
+  localStorage.setItem(`maxscore`,0);
+  console.log(`eser`)
+  score =oldMaxScore= 0;
+  setMaxScore();
+}
+
+const setMaxScore = () => {
+  oldMaxScore = localStorage.getItem(`maxscore`);
+  if (!oldMaxScore) oldMaxScore = 0;
+  let maxScore = oldMaxScore > score ? oldMaxScore : score;
+  localStorage.setItem(`maxscore`, maxScore);
+  maxScoreText.textContent =maxScore;
+}
